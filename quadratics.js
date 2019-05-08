4f7ac44f8a4e05bbd9b578bfdc8b0cbc6995d471
Bubadabado <adam@adamrobertson.us>
2019-05-08 10:02:49 -0500

    added things to the sketch files, fixed bugs, began style page

--- quadratics.js
+++ quadratics.js
@@ -1,27 +1,37 @@
 var aValue;
 var q;
+var aValue;
+var translateX;
+var translateY;
+var flipX;
+var flipy;
+var canvas;
 
 function setup() {
-    createCanvas(800, 800);
+    canvas = createCanvas(800, 800);
+    var cx = (windowWidth - width) / 2;
+    var cy = (windowHeight - height) / 2;
+    canvas.position(cx, cy);
     aValue = createSlider(-8, 8, 0, 1);
-    aValue.position(10, 10);
+    aValue.position(cx + 15, cy + 10);
     aValue.style('width', '120px');
-    translateX = createSlider(-1 * (width/2), width/2, 1, 50);
-    translateX.position(10, 35);
+    translateX = createSlider(-1 * ((width - 100) / 2), (width - 100) / 2, 1, 50);
+    translateX.position(cx + 15, cy + 35);
     translateX.style('width', '120px');
-    translateY = createSlider(-1 * (height/2), height/2, 1, 50);
-    translateY.position(10, 60);
+    translateY = createSlider(-1 * ((height - 100) / 2), (height - 100) / 2, 1, 50);
+    translateY.position(cx + 15, cy + 60);
     translateY.style('width', '120px');
     flipX = createSlider(0, 1, 0, 1);
-    flipX.position(10, 85);
+    flipX.position(cx + 15, cy + 85);
     flipX.style('width', '120px');
     flipY = createSlider(0, 1, 0, 1);
-    flipY.position(10, 110);
+    flipY.position(cx + 15, cy + 110);
     flipY.style('width', '120px');
-    q = new quadratic(width/2, (-height) - height/2, width * 0.25, height*4, 0, PI); //parent var q = new quadratic(width/2, 0, width * 0.25, height, 0, PI);
+    q = new quadratic(width / 2, (-height) - height / 2, width * 0.25, height * 4, 0, PI); //parent var q = new quadratic(width/2, 0, width * 0.25, height, 0, PI);
 }
 
 function draw() {
+    positionCanvas();
     var m = new mouse();
     background(255);
     arcSetup();
@@ -29,17 +39,16 @@ function draw() {
     textSetup();
     drawCoords();
     drawSliderText();
-    //mininum
-    if (mouseY > height * 0.85 && mouseX > width/3 && mouseX < width * 0.66) {
-        textSize(width / 20);
-        fill(0);
-        strokeWeight(5);
-        stroke(255);
-        text("minimum:", width / 2, height * 0.9);
+    if (mouseY > height / 2 + translateY.value() - 20 && mouseX > width / 2 + translateX.value() - 20 && mouseY < height / 2 + translateY.value() + 20 && mouseX < width / 2 + translateX.value() + 20) {
+        textAlign(CENTER);
+        //text("Vertex", translateX.value() + width/2, translateY.value() + height/2 + 50);
+        //strokeWeight(2);
+        line(0, translateY.value() + height / 2, width, translateY.value() + height / 2);
+        line(translateX.value() + width / 2, 0, translateX.value() + width / 2, height);
     }
     m.show();
     var stretch = aValue.value();
-    if(stretch != 0) {
+    if (stretch != 0) {
         q.setA(stretch);
     }
     q.setTransformX(translateX.value());
@@ -47,6 +56,16 @@ function draw() {
     q.setDir(flipY.value());
 }
 
+function positionCanvas() {
+    var cx = (windowWidth - width) / 2;
+    var cy = (windowHeight - height) / 2;
+    canvas.position(cx, cy);
+    aValue.position(cx + 15, cy + 10);
+    translateX.position(cx + 15, cy + 35);
+    translateY.position(cx + 15, cy + 60);
+    flipX.position(cx + 15, cy + 85);
+    flipY.position(cx + 15, cy + 110);
+}
 function arcSetup() {
     stroke(0);
     strokeWeight(2);
@@ -62,35 +81,55 @@ function textSetup() {
 
 function drawSliderText() {
     textAlign(LEFT);
-    text("A value", 140, 28)
-    text("Translate X", 140, 52)
-    text("Translate Y", 140, 76)
-    text("Flip horizontally", 140, 100)
-    text("Flip vertically", 140, 124)
+    if (aValue.value() == 0) {
+        text("A value: 1", 140, 28)
+    } else if (aValue.value() > 1) {
+        text("A value: " + parseFloat(1 / aValue.value()).toFixed(2), 140, 28);
+    } else {
+        text("A value: " + Math.abs(aValue.value()), 140, 28);
+    }
+    text("Translate X: " + translateX.value() / 50, 140, 52);
+    text("Translate Y: " + -translateY.value() / 50, 140, 76);
+    text("Flip horizontally", 140, 100);
+    text("Flip vertically", 140, 124);
+    textAlign(RIGHT);
+    if (flipY.value() == 0) {
+        text("Min: " + -translateY.value() / 50, width - 10, 28);
+        text("Max: ∞", width - 10, 52);
+        text("Domain: (-∞, ∞)", width - 10, 76);
+        text("range: [" + -translateY.value() / 50 + ", ∞)", width - 10, 100);
+    } else {
+        text("Min: -∞", width - 10, 28);
+        text("Max: " + -translateY.value() / 50, width - 10, 52);
+        text("Domain: (-∞, ∞)", width - 10, 76);
+        text("range: ( -∞, " + -translateY.value() / 50 + "]", width - 10, 100);
+    }
+
+    text("Vertex : (" + translateX.value() / 50 + ", " + -translateY.value() / 50 + ")", width - 10, 124);
 }
 
 function drawCoords() {
     //height
-    for(var i = 0; i < height; i += 50) {
-        if(i > 0) {
-            line(width/2 - 5, i, width/2  + 5, i);
-            if(((i - height/2) / 50) != 0) {
-                text((i - height/2) / 50, width/2 + 20, i + 5);
+    for (var i = 0; i < height; i += 50) {
+        if (i > 0) {
+            line(width / 2 - 5, i, width / 2 + 5, i);
+            if (((i - height / 2) / 50) != 0) {
+                text(-((i - height / 2) / 50), width / 2 + 20, i + 5);
             }
         }
     }
     //width
-    for(i = 0; i < width; i += 50) {
-        if(i > 0) {
-            line(i, height/2 + 5, i, height/2 - 5);
-            if(((i - width/2) / 50) != 0) {
-                text((i - width/2) / 50, i, height/2 - 15);
+    for (i = 0; i < width; i += 50) {
+        if (i > 0) {
+            line(i, height / 2 + 5, i, height / 2 - 5);
+            if (((i - width / 2) / 50) != 0) {
+                text((i - width / 2) / 50, i, height / 2 - 15);
             }
-            
+
         }
     }
-    line(0, height/2, width, height/2)
-    line(width/2, 0, width/2, height)
+    line(0, height / 2, width, height / 2);
+    line(width / 2, 0, width / 2, height);
 }
 class quadratic {
     constructor(x, y, width, height, p1, p2) {
@@ -107,8 +146,8 @@ class quadratic {
     }
 
     show() {
-        if(this.dir == 0) {
-            if(this.a > 0) {
+        if (this.dir == 0) {
+            if (this.a > 0) {
                 arc(this.x + this.tx, this.y + this.ty, this.width * this.a, this.height, this.p1, this.p2);
             }
             else {
@@ -116,12 +155,11 @@ class quadratic {
             }
         }
         else {
-            if(this.a > 0) {
+            if (this.a > 0) {
                 arc(this.x + this.tx, this.y + this.ty + height * 4, this.width * this.a, this.height, PI, 0);
-                print(this.y + this.ty + height * 1.5);
             }
             else {
-                arc(this.x + this.tx, this.y + this.ty, this.width / this.a, this.height, PI, 0);
+                arc(this.x + this.tx, this.y + this.ty + height * 4, this.width / this.a, this.height, PI, 0);
             }
         }
     }
